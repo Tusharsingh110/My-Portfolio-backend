a21d8870fc626bd2322d65af59aa9e0397b56bed
Tusharsingh110 <tusharsingh6t@gmail.com>
2023-09-09 11:12:22 +0530

    added vercl.json updated added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const connectToMongo = require('./db')
 const express = require('express')
+const cors = require('cors')
 connectToMongo();
 
 const app = express();
@@ -8,6 +9,7 @@ const port = 3000;
 app.get('/',(req,res)=> {
     res.send('Hello World')
 })
+app.use(cors())
 app.use(express.json());
 
 app.use('/api/createFeedback', require('./routes/Testing'))
